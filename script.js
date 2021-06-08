ec655d3dc283395913962596e908ba3daf0945dc
shannonssd <shannonssd@gmail.com>
2021-06-08 13:33:14 +0800

    Added feature to prompt username entry, return output game instructions, collect username and display username in game results.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// Create Scissors Paper Stone Game with Win-Loss Record and formatting
+// Create Scissors Paper Stone Game with User Name Inclusion
 
 // Create global variable to track user and computer win as well as no of draws
 var userWin = 0;
 var compWin = 0;
 var draws = 0;
+
+// Create game mode variable to 1. take in user name as input 2. switch to SPS hame
+var gameMode = "waiting for user to enter name";
+// Create username variable to take in user's input
+var userName = "";
 
 // Create function which generates random number
 var generateRandomNum = function () {
@@ -28,50 +33,57 @@ var assignNumToOptions = function () {
   return programOutput;
 };
 
-var main = function (userGuess) {
+var main = function (input) {
   // Call function which generates computer's choice into main function
   var computerChoice = assignNumToOptions();
   // Create default statement
   var myOutputValue = "";
   // Create condition where user loses
-  if (
-    (userGuess == "scissors" && computerChoice == "stone") ||
-    (userGuess == "paper" && computerChoice == "scissors") ||
-    (userGuess == "stone" && computerChoice == "paper")
-  ) {
-    // Update value of global variable for computer win count
-    compWin = compWin + 1;
-    // Create variable which outputs user win percentage
-    var winPercentage = (userWin / (userWin + compWin + draws)) * 100;
-    myOutputValue = `Sorry, you lost! 😿 <br> You chose ${userGuess} <br>  The computer chose ${computerChoice} <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br> <br>Please try again! ✂️🧻🗿`;
-  }
+  // Take in username
+  if (gameMode == "waiting for user to enter name") {
+    userName = input;
+    myOutputValue = `Hello ${userName}! <br> Are you ready to play? <br> You can play by entering either: <br> <br> ✂️ scissors  <br><br>🧻 paper  <br><br> 🗿 stone `;
+    // Switch to SPS game
+    gameMode = "SPS";
+  } else if (gameMode == "SPS") {
+    if (
+      (input == "scissors" && computerChoice == "stone") ||
+      (input == "paper" && computerChoice == "scissors") ||
+      (input == "stone" && computerChoice == "paper")
+    ) {
+      // Update value of global variable for computer win count
+      compWin = compWin + 1;
+      // Create variable which outputs user win percentage
+      var winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+      myOutputValue = `Sorry ${userName}, you lost! 😿 <br> You chose ${input} <br>  The computer chose ${computerChoice} <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br> <br>Please try again! ✂️🧻🗿`;
+    }
 
-  // Create condition where scissors beats paper and user wins
-  // Create condition where paper beats stone and user wins
-  // Create condition where stone beats scissors and user wins
-  if (
-    (userGuess == "scissors" && computerChoice == "paper") ||
-    (userGuess == "paper" && computerChoice == "stone") ||
-    (userGuess == "stone" && computerChoice == "scissors")
-  ) {
-    // Update value of global variable for user win count
-    userWin = userWin + 1;
-    // ?Call win percentage variable into winning conditional
-    winPercentage = (userWin / (userWin + compWin + draws)) * 100;
-    myOutputValue = `Congratulations, you won! 😁 <br> You chose ${userGuess} <br> The computer chose ${computerChoice}. <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%.  <br> <br>Wanna try again? ✂️🧻🗿`;
-  }
-  // Create condition where input = program output and result is a draw
-  if (userGuess == computerChoice) {
-    // Update value of global variable for draws count
-    draws = draws + 1;
-    // ?Call win percentage variable into winning conditional
-    winPercentage = (userWin / (userWin + compWin + draws)) * 100;
-    myOutputValue = `Darn, it's a draw! 😡 <br> You chose ${userGuess} <br> The computer chose the same! <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br><br> Please try again! ✂️🧻🗿`;
-  }
-  // If user types anything other than "scissors", "paper" or "stone", inform them that only these 3 words are vaild options
-  if (userGuess != "scissors" && userGuess != "paper" && userGuess != "stone") {
-    myOutputValue =
-      "Sorry, you entered an invalid option! 😅 <br> Please enter one of the following options: <br> <br> ✂️ scissors  <br><br>🧻 paper  <br><br> 🗿 stone ";
+    // Create condition where scissors beats paper and user wins
+    // Create condition where paper beats stone and user wins
+    // Create condition where stone beats scissors and user wins
+    if (
+      (input == "scissors" && computerChoice == "paper") ||
+      (input == "paper" && computerChoice == "stone") ||
+      (input == "stone" && computerChoice == "scissors")
+    ) {
+      // Update value of global variable for user win count
+      userWin = userWin + 1;
+      // ?Call win percentage variable into winning conditional
+      winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+      myOutputValue = `Congratulations ${userName}, you won! 😁 <br> You chose ${input} <br> The computer chose ${computerChoice}. <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%.  <br> <br>Wanna try again? ✂️🧻🗿`;
+    }
+    // Create condition where input = program output and result is a draw
+    if (input == computerChoice) {
+      // Update value of global variable for draws count
+      draws = draws + 1;
+      // ?Call win percentage variable into winning conditional
+      winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+      myOutputValue = `Darn ${userName}, it's a draw! 😡 <br> You chose ${input} <br> The computer chose the same! <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br><br> Please try again! ✂️🧻🗿`;
+    }
+    // If user types anything other than "scissors", "paper" or "stone", inform them that only these 3 words are vaild options
+    if (input != "scissors" && input != "paper" && input != "stone") {
+      myOutputValue = `Sorry ${userName}, you entered an invalid option! 😅 <br> Please enter one of the following options: <br> <br> ✂️ scissors  <br><br>🧻 paper  <br><br> 🗿 stone `;
+    }
   }
   return myOutputValue;
 };
