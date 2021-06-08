e5b32aa9a3f6a554d845a82d7de48b2e5cc2de83
shannonssd <shannonssd@gmail.com>
2021-06-08 12:45:14 +0800

    Added win, draw, loss, win percetage and formatting.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Create Scissors Paper Stone Game with reverse rules!
+// Create Scissors Paper Stone Game with Win-Loss Record and formatting
+
+// Create global variable to track user and computer win as well as no of draws
+var userWin = 0;
+var compWin = 0;
+var draws = 0;
 
 // Create function which generates random number
 var generateRandomNum = function () {
@@ -24,42 +29,49 @@ var assignNumToOptions = function () {
 };
 
 var main = function (userGuess) {
-  // Create variable where user guess is turned into string which only outputs scissors/paper/stone
-  if (userGuess == "reversed scissors") {
-    var playerChoice = "scissors";
-  }
-  if (userGuess == "reversed paper") {
-    playerChoice = "paper";
-  }
-  if (userGuess == "reversed stone") {
-    playerChoice = "stone";
-  }
   // Call function which generates computer's choice into main function
   var computerChoice = assignNumToOptions();
-  // Create defaults statement where user loses
-  var myOutputValue = `Sorry, you lost reverse Scissors Paper Stone 😂  <br> You chose ${playerChoice} <br>  The computer chose ${computerChoice} <br> <br>Please try again! ✂️🧻🗿`;
-  // Create condition where scissors beats stone and user wins
-  // Create condition where paper beats scissors and user wins
-  // Create condition where stone beats paper and user wins
+  // Create default statement
+  var myOutputValue = "";
+  // Create condition where user loses
   if (
-    (playerChoice == "scissors" && computerChoice == "stone") ||
-    (playerChoice == "paper" && computerChoice == "scissors") ||
-    (playerChoice == "stone" && computerChoice == "paper")
+    (userGuess == "scissors" && computerChoice == "stone") ||
+    (userGuess == "paper" && computerChoice == "scissors") ||
+    (userGuess == "stone" && computerChoice == "paper")
   ) {
-    myOutputValue = `Congratulations, you won reverse Scissors Paper Stone! 😁 <br> You chose ${playerChoice} <br> The computer chose ${computerChoice}  <br> <br>Wanna try again? ✂️🧻🗿`;
+    // Update value of global variable for computer win count
+    compWin = compWin + 1;
+    // Create variable which outputs user win percentage
+    var winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+    myOutputValue = `Sorry, you lost! 😿 <br> You chose ${userGuess} <br>  The computer chose ${computerChoice} <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br> <br>Please try again! ✂️🧻🗿`;
+  }
+
+  // Create condition where scissors beats paper and user wins
+  // Create condition where paper beats stone and user wins
+  // Create condition where stone beats scissors and user wins
+  if (
+    (userGuess == "scissors" && computerChoice == "paper") ||
+    (userGuess == "paper" && computerChoice == "stone") ||
+    (userGuess == "stone" && computerChoice == "scissors")
+  ) {
+    // Update value of global variable for user win count
+    userWin = userWin + 1;
+    // ?Call win percentage variable into winning conditional
+    winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+    myOutputValue = `Congratulations, you won! 😁 <br> You chose ${userGuess} <br> The computer chose ${computerChoice}. <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%.  <br> <br>Wanna try again? ✂️🧻🗿`;
   }
   // Create condition where input = program output and result is a draw
-  if (playerChoice == computerChoice) {
-    myOutputValue = `Darn, it's a draw! 😡 <br> You chose ${playerChoice} <br> The computer chose the same! <br><br> Please try again! ✂️🧻🗿`;
+  if (userGuess == computerChoice) {
+    // Update value of global variable for draws count
+    draws = draws + 1;
+    // ?Call win percentage variable into winning conditional
+    winPercentage = (userWin / (userWin + compWin + draws)) * 100;
+    myOutputValue = `Darn, it's a draw! 😡 <br> You chose ${userGuess} <br> The computer chose the same! <br> You've won: ${userWin} times. <br> The computer has won ${compWin} times. <br> You have drawn: ${draws} times. <br> Your win percentage is ${winPercentage}%. <br><br> Please try again! ✂️🧻🗿`;
   }
-  // If user types anything other than "reversed scissors", "reversed paper" or "reversed stone", inform them that only these 3 words are vaild options
-  if (
-    userGuess != "reversed scissors" &&
-    userGuess != "reversed paper" &&
-    userGuess != "reversed stone"
-  ) {
+  // If user types anything other than "scissors", "paper" or "stone", inform them that only these 3 words are vaild options
+  if (userGuess != "scissors" && userGuess != "paper" && userGuess != "stone") {
     myOutputValue =
-      "Sorry, you entered an invalid option! 😅 <br> Please enter one of the following options: <br> <br> ✂️ reversed scissors  <br><br>🧻 reversed paper  <br><br> 🗿 reversed stone ";
+      "Sorry, you entered an invalid option! 😅 <br> Please enter one of the following options: <br> <br> ✂️ scissors  <br><br>🧻 paper  <br><br> 🗿 stone ";
   }
   return myOutputValue;
 };
